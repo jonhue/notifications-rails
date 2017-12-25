82b22660ce220da11857226c4d4588dcf9b876b9
Jonas Hübotter <jonas.huebotter@gmx.de>
2017-12-25 20:00:57 +0100

    minor improvements

--- notification-handler/lib/generators/templates/install/initializer.rb
+++ notification-handler/lib/generators/templates/install/initializer.rb
@@ -3,4 +3,8 @@ NotificationHandler.configure do |config|
     # ...
     # config.placeholder = true
 
+    # Groups are a powerful way to bulk-create notifications for multiple objects that don't necessarily have a common class.
+    # Learn more: https://github.com/jonhue/notifications-rails/tree/master/notification-handler#groups
+    # config.define_group :subscribers, User.where(subscriber: true)
+
 end
