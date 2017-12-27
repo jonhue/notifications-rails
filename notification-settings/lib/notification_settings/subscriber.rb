de5fbda5fb8322fba7b35ca1a3c4a06729f6df00
Jonas Hübotter <jonas.huebotter@gmx.de>
2017-12-27 17:48:55 +0100

    Update subscriber.rb

--- notification-settings/lib/notification_settings/subscriber.rb
+++ notification-settings/lib/notification_settings/subscriber.rb
@@ -8,6 +8,7 @@ module NotificationSettings
         module ClassMethods
             def notification_subscriber
                 has_many :notification_subscriptions, as: :subscriber, class_name: 'NotificationSettings::Subscription', dependent: :destroy
+                has_many :notification_subscribables, through: :notification_subscriptions, source: :subscribable
                 include NotificationSettings::Subscriber::InstanceMethods
             end
         end
@@ -16,6 +17,11 @@ module NotificationSettings
         
             def subscribe options = {}
                 NotificationSettings::Subscription.create subscriber: self, options
+            end
+            
+            def unsubscribe subscribable
+                subscription = self.notification_subscriptions.find_by subscribable_id: subscribable.id, subscribable_type: subscribable.class.to_s
+                subscription.destroy
             end
 
         end
