69facd1713795ef0fffa041e6b21340ede88bb65
Jonas Hübotter <jonas.huebotter@gmx.de>
2017-12-27 18:31:41 +0100

    Update object.rb

--- notification-settings/lib/notification_settings/object.rb
+++ notification-settings/lib/notification_settings/object.rb
@@ -1,27 +1,15 @@
 module NotificationSettings
     module Object
 
-        def self.included base
-            base.extend ClassMethods
-        end
+        has_one :notification_setting, as: :object, class_name: 'NotificationSettings::Setting', dependent: :destroy
+        include NotificationSettings::Object::InstanceMethods
 
-        module ClassMethods
-            def notification_settings
-                has_one :notification_setting, as: :object, class_name: 'NotificationSettings::Setting', dependent: :destroy
-                include NotificationSettings::Object::InstanceMethods
+        after_create_commit :create_notification_setting
 
-                after_create_commit :create_notification_setting
-            end
-        end
+        private
 
-        module InstanceMethods
-
-            private
-
-            def create_notification_setting
-                self.notification_setting.create!
-            end
-
+        def create_notification_setting
+            self.notification_setting.create!
         end
 
     end
