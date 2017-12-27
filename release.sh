aee424d4d7ec05a67a4ebf7d7c35a05ba7a6d4b3
Jonas Hübotter <jonas.huebotter@gmx.de>
2017-12-27 23:27:10 +0100

    add release.sh

---
+++ release.sh
@@ -0,0 +1,8 @@
+gem push notification-handler-1.0.0.beta4.gem
+gem push notification-pusher-1.0.0.beta4.gem
+gem push notification-pusher-actioncable-1.0.0.beta4.gem
+gem push notification-pusher-actionmailer-1.0.0.beta4.gem
+gem push notification-pusher-onesignal-1.0.0.beta4.gem
+gem push notification-renderer-1.0.0.beta4.gem
+gem push notification-settings-1.0.0.beta4.gem
+gem push notifications-rails-1.0.0.beta4.gem
