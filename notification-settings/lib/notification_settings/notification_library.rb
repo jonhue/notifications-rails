8cfc9adea24caa9301819c937158eb4af531227a
Jonas Hübotter <jonas.huebotter@gmx.de>
2017-12-27 16:32:08 +0100

    Update notification_library.rb

--- notification-settings/lib/notification_settings/notification_library.rb
+++ notification-settings/lib/notification_settings/notification_library.rb
@@ -1,20 +1,44 @@
 module NotificationSettings
     module NotificationLibrary
 
-        before_create :validate_status
+        before_create :validate_create
 
         private
 
-        def validate_status_create
-            self.target.notification_setting.present? && NotificationSettings.configuration.do_not_notify_statuses.include?(self.target.notification_setting.status) ? false : true
+        def validate_create
+            valid = true
+            
+            if self.target.notification_setting.present?
+                # Status
+                valid = false if NotificationSettings.configuration.do_not_notify_statuses.include?(self.target.notification_setting.status)
+                # Category
+                valid = false if !self.target.notification_setting.settings.dig(:enabled) || self.target.notification_setting.settings.dig(self.category.to_sym, :enabled)
+            end
+            
+            valid
         end
 
-        def validate_status_push
-            self.target.notification_setting.present? && NotificationSettings.configuration.do_not_push_statuses.include?(self.target.notification_setting.status) ? false : true
+        def validate_push
+            valid = true
+            
+            if self.target.notification_setting.present?
+                # Status
+                valid = false if NotificationSettings.configuration.do_not_push_statuses.include?(self.target.notification_setting.status)
+                # Category & Pusher
+                if self.push.kind_of?(Array)
+                    self.push.each do |pusher|
+                        valid = false if !self.target.notification_setting.settings.dig(pusher.to_sym) || self.target.notification_setting.settings.dig(self.category.to_sym, pusher.to_sym) || ( !self.target.notification_setting.settings.dig(:index) && self.target.notification_setting.settings.dig(pusher.to_sym).nil? ) || ( !self.target.notification_setting.settings.dig(self.category.to_sym, :index) && self.target.notification_setting.settings.dig(self.category.to_sym, pusher.to_sym).nil? )
+                    end
+                else
+                    valid = false if !self.target.notification_setting.settings.dig(self.push.to_sym) || self.target.notification_setting.settings.dig(self.category.to_sym, self.push.to_sym) || ( !self.target.notification_setting.settings.dig(:index) && self.target.notification_setting.settings.dig(self.push.to_sym).nil? ) || ( !self.target.notification_setting.settings.dig(self.category.to_sym, :index) && self.target.notification_setting.settings.dig(self.category.to_sym, self.push.to_sym).nil? )
+                end
+            end
+            
+            valid
         end
 
         def initialize_pusher
-            return unless validate_status_push
+            return unless validate_push
             super
         end
 
