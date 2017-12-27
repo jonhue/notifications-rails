aae9a11ba45d8b8ada85269ac6be5c01edc5d700
Jonas Hübotter <jonas.huebotter@gmx.de>
2017-12-27 18:30:32 +0100

    Update target.rb

--- notification-handler/lib/notification_handler/target.rb
+++ notification-handler/lib/notification_handler/target.rb
@@ -3,14 +3,15 @@ module NotificationHandler
 
         def self.included base
             base.extend ClassMethods
-            base.extend NotificationSettings::Object if defined?(NotificationSettings)
-            base.extend NotificationSettings::Subscriber if defined?(NotificationSettings)
         end
 
         module ClassMethods
             def notification_target
                 has_many :notifications, as: :target, dependent: :destroy
                 include NotificationHandler::Target::InstanceMethods
+                
+                extend NotificationSettings::Object if defined?(NotificationSettings)
+                extend NotificationSettings::Subscriber if defined?(NotificationSettings)
             end
         end
 
