71bbdcdedce67615048960ae93376a5d3658f952
Jonas Hübotter <jonas.huebotter@gmx.de>
2017-12-27 18:32:03 +0100

    Update railtie.rb

--- notification-settings/lib/notification_settings/railtie.rb
+++ notification-settings/lib/notification_settings/railtie.rb
@@ -3,13 +3,7 @@ require 'railties'
 module NotificationSettings
     class Railtie < Rails::Railtie
 
-        initializer 'notification-settings.active_record' do
-            ActiveSupport.on_load :active_record do
-                include NotificationSettings::Object
-                include NotificationSettings::Subscriber
-                include NotificationSettings::Subscribable
-            end
-        end
+        # ...
 
     end
 end
