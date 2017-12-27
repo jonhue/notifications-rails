fc55b897fafd345f966e7d7afea6de2ad1da7fb7
Jonas Hübotter <jonas.huebotter@gmx.de>
2017-12-27 18:03:37 +0100

    Update notification_library.rb

--- notification-settings/lib/notification_settings/notification_library.rb
+++ notification-settings/lib/notification_settings/notification_library.rb
@@ -2,6 +2,8 @@ module NotificationSettings
     module NotificationLibrary
 
         before_create :validate_create
+        
+        belongs_to :subscription, class_name: 'NotificationSettings::Subscription', optional: true
 
         private
 
@@ -16,12 +18,6 @@ module NotificationSettings
                 valid = false if !self.target.notification_setting.settings.dig(:enabled)
                 ## Category
                 valid = false if !self.target.notification_setting.category_settings.dig(self.category.to_sym, :enabled)
-            end
-            if self.subscription.present?
-                # Settings
-                valid = false if !self.subscription.notification_setting.settings.dig(:enabled)
-                ## Category
-                valid = false if !self.subscription.notification_setting.category_settings.dig(self.category.to_sym, :enabled)
             end
             
             valid
@@ -45,19 +41,6 @@ module NotificationSettings
                     valid = false if !self.target.notification_setting.settings.dig(self.push.to_sym) || ( !self.target.notification_setting.settings.dig(:index) && self.target.notification_setting.settings.dig(self.push.to_sym).nil? )
                     ## Category
                     valid = false if !self.target.notification_setting.category_settings.dig(self.category.to_sym, self.push.to_sym) || ( !self.target.notification_setting.category_settings.dig(self.category.to_sym, :index) && self.target.notification_setting.category_settings.dig(self.category.to_sym, self.push.to_sym).nil? )
-                end
-            end
-            if self.subscription.present?
-                if self.push.kind_of?(Array)
-                    self.push.each do |pusher|
-                        valid = false if !self.subscription.notification_setting.settings.dig(pusher.to_sym) || ( !self.subscription.notification_setting.settings.dig(:index) && self.subscription.notification_setting.settings.dig(pusher.to_sym).nil? )
-                        ## Category
-                        valid = false if !self.subscription.notification_setting.category_settings.dig(self.category.to_sym, pusher.to_sym) || ( !self.subscription.notification_setting.category_settings.dig(self.category.to_sym, :index) && self.subscription.notification_setting.category_settings.dig(self.category.to_sym, pusher.to_sym).nil? )
-                    end
-                else
-                    valid = false if !self.subscription.notification_setting.settings.dig(self.push.to_sym) || ( !self.subscription.notification_setting.settings.dig(:index) && self.subscription.notification_setting.settings.dig(self.push.to_sym).nil? )
-                    ## Category
-                    valid = false if !self.subscription.notification_setting.category_settings.dig(self.category.to_sym, self.push.to_sym) || ( !self.subscription.notification_setting.category_settings.dig(self.category.to_sym, :index) && self.subscription.notification_setting.category_settings.dig(self.category.to_sym, self.push.to_sym).nil? )
                 end
             end
             
