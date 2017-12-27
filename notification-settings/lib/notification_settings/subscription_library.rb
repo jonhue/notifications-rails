358d465688337dcf6cdc05014c1c6382cd03f212
Jonas Hübotter <jonas.huebotter@gmx.de>
2017-12-27 18:09:20 +0100

    Update subscription_library.rb

--- notification-settings/lib/notification_settings/subscription_library.rb
+++ notification-settings/lib/notification_settings/subscription_library.rb
@@ -1,11 +1,6 @@
 module NotificationSettings
     module SubscriptionLibrary
         
-        has_many :notifications, class_name: '::Notification'
-        has_one :notification_setting, class_name: 'Setting'
-        
-        after_create_commit :create_notification_setting
-        
         private
         
         def create_notification_setting
