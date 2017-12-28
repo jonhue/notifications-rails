7f60ec79c81dd34253ea08c884a68ce6f8580d8c
Jonas Hübotter <jonas.huebotter@gmx.de>
2017-12-28 12:57:27 +0100

    minor bugfixes

--- notification-handler/lib/notification_handler/group.rb
+++ notification-handler/lib/notification_handler/group.rb
@@ -4,6 +4,11 @@ module NotificationHandler
         attr_accessor :name
         attr_accessor :target_scope
 
+        def initialize name, target_scope
+            @name = name
+            @target_scope = target_scope
+        end
+
         def find_by_name name
             ObjectSpace.each_object(NotificationHandler::Group).select { |group| group.name == name.to_sym }
         end
