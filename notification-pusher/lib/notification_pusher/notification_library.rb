8f02beea4bdedb021d538e78da2cea70de250672
Jonas Hübotter <jonas.huebotter@gmx.de>
2017-12-28 17:08:42 +0100

    minor bugfixes

--- notification-pusher/lib/notification_pusher/notification_library.rb
+++ notification-pusher/lib/notification_pusher/notification_library.rb
@@ -26,17 +26,21 @@ module NotificationPusher
                 unless self.push.nil?
                     if self.push.kind_of?(Array)
                         self.push.each do |class_name|
-                            pusher = NotificationPusher::Pusher.find_by_name(class_name).last
+                            pusher = NotificationPusher::Pusher.find_by_name(class_name).first
                             if defined?(NotificationPusher.const_get(pusher.name))
-                                options = pusher.options.merge! self.push_options[class_name.to_sym]
-                                # pusher.instances << NotificationPusher.const_get(pusher.name).new self, options
+                                default_options = pusher.options
+                                options = default_options.merge! self.push_options[class_name.to_sym]
+                                # instance = NotificationPusher.const_get(pusher.name).new self, options
+                                # pusher.instances << instance
                             end
                         end
                     else
-                        pusher = NotificationPusher::Pusher.find_by_name(self.push).last
+                        pusher = NotificationPusher::Pusher.find_by_name(self.push).first
                         if defined?(NotificationPusher.const_get(pusher.name))
-                            options = pusher.options.merge! self.push_options
-                            # pusher.instances << NotificationPusher.const_get(pusher.name).new self, options
+                            default_options = pusher.options
+                            options = default_options.merge! self.push_options
+                            # instance = NotificationPusher.const_get(pusher.name).new self, options
+                            # pusher.instances << instance
                         end
                     end
                 end
