cea6f05f5d2d303ea73b32dbe680c6f2aad41abe
Jonas Hübotter <jonas.huebotter@gmx.de>
2017-12-29 18:59:39 +0100

    Update one_signal.rb

--- notification-pusher/notification-pusher-onesignal/lib/notification_pusher/one_signal.rb
+++ notification-pusher/notification-pusher-onesignal/lib/notification_pusher/one_signal.rb
@@ -8,6 +8,8 @@ class NotificationPusher::OneSignal
                 app_id: options[:app_id],
                 url: options[:url] || notification.metadata[:onesignal_url],
                 contents: options[:contents] || notification.metadata[:onesignal_contents].to_h,
+                headers: options[:headers] || notification.metadata[:onesignal_headers].to_h,
+                subtitle: options[:subtitle] || notification.metadata[:onesignal_subtitle].to_h,
                 include_player_ids: options[:player_ids]
     		}, opts: { auth_key: options[:auth_key] }
         end
