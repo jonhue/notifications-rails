9054da09369cd44295b983ec655c673d4c750edd
Tyler Rick <tyler@tylerrick.com>
2019-01-13 22:15:45 -0800

    Fix error when calling Subscriber#subscribe:
      NoMethodError:
        undefined method `create!' for nil:NilClass
    
        18: def create_notification_setting
     => 19:   notification_setting.create!
        20: end
    
    Change to use before_create :build_notification_setting, and automatically build
    `notification_setting` if object doesn't have one yet like we did in #40.

--- notification-settings/lib/notification_settings/subscription_library.rb
+++ notification-settings/lib/notification_settings/subscription_library.rb
@@ -7,16 +7,14 @@ module NotificationSettings
     extend ActiveSupport::Concern
 
     included do
-      after_create_commit :create_notification_setting
+      before_create :build_notification_setting
 
       include NotificationSettings::SubscriptionLibrary::InstanceMethods
     end
 
     module InstanceMethods
-      private
-
-      def create_notification_setting
-        notification_setting.create!
+      def notification_setting
+        super || build_notification_setting
       end
     end
   end
