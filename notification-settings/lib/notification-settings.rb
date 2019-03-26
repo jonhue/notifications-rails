50e479566c4fa3a0b7714fc12f3b6cf401c5f32c
Federico Gonzalez <fedegl@pm.me>
2019-03-26 00:12:04 -0400

    Use create_notification_setting! method on after_create_commit callback

--- notification-settings/lib/notification-settings.rb
+++ notification-settings/lib/notification-settings.rb
@@ -9,7 +9,6 @@ module NotificationSettings
   autoload :Subscriber, 'notification_settings/subscriber'
   autoload :Subscribable, 'notification_settings/subscribable'
   autoload :SettingLibrary, 'notification_settings/setting_library'
-  autoload :SubscriptionLibrary, 'notification_settings/subscription_library'
   autoload :NotificationLibrary, 'notification_settings/notification_library'
   autoload :NotificationScopes, 'notification_settings/notification_scopes'
 end
