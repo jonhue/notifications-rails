92b37b77a7b4d8f0d42b2a8b8675de24d68a909d
Jonas Hübotter <me@jonhue.me>
2019-05-31 23:06:15 +0200

    small fixes

--- notification-settings/spec/support/rails_app/db/schema.rb
+++ notification-settings/spec/support/rails_app/db/schema.rb
@@ -17,7 +17,6 @@ ActiveRecord::Schema.define(version: 2019_05_09_202305) do
     t.integer "subscriber_id"
     t.string "subscribable_type"
     t.integer "subscribable_id"
-    t.string "status"
     t.text "settings"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
