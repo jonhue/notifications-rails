cb94e402b8b7927c5554d96a4677368cf7b9d9ec
Jonas Hübotter <me@jonhue.me>
2019-06-06 08:41:55 +0200

    [#105] Complete documentation of notification_target & notification_object (#106)

--- notification-handler/lib/notification_handler/target.rb
+++ notification-handler/lib/notification_handler/target.rb
@@ -1,20 +1,22 @@
 # frozen_string_literal: true
 
+require 'active_support'
+
 module NotificationHandler
   module Target
-    def self.included(base)
-      base.extend(ClassMethods)
-    end
+    extend ActiveSupport::Concern
 
-    module ClassMethods
-      def notification_target
+    included do
+      def self.notification_target
         has_many :notifications, as: :target, dependent: :destroy
         include NotificationHandler::Target::InstanceMethods
 
         include NotificationSettings::Target if defined?(NotificationSettings)
-        return unless defined?(NotificationSettings)
-
-        include NotificationSettings::Subscriber
+        # rubocop:disable Style/GuardClause
+        if defined?(NotificationSettings)
+          include NotificationSettings::Subscriber
+        end
+        # rubocop:enable Style/GuardClause
       end
     end
 
