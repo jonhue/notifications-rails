9f388630c742a77cf4b5de2697d179970124c9d1
Jonas Hübotter <jonas.huebotter@gmail.com>
2020-04-22 08:43:19 +0200

    [#179] Fix rubocop offenses (#180)

--- notification-renderer/spec/helpers/notification_renderer_helper_spec.rb
+++ notification-renderer/spec/helpers/notification_renderer_helper_spec.rb
@@ -1,19 +1,26 @@
-require_relative '../rails_helper' 
+# frozen_string_literal: true
 
-RSpec.describe NotificationRendererHelper, :type => :helper do
+require_relative '../rails_helper'
+
+RSpec.describe NotificationRendererHelper, type: :helper do
   describe 'render_notifications' do
     it 'renders notifications using default renderer' do
-        notification1 = create_title_and_content_notification('title1', 'content1')
-        notification2 = create_title_and_content_notification('title2', 'content2')
+      notification1 =
+        create_title_and_content_notification('title1', 'content1')
+      notification2 =
+        create_title_and_content_notification('title2', 'content2')
 
-        rendered = helper.render_notifications([notification1, notification2])
+      rendered = helper.render_notifications([notification1, notification2])
 
-        # see notification render format at ../support/rails_app/app/views/notifications/title_and_content/_index.html.erb
-        expect(rendered).to eq('<div class="notification-renderer notifications"><div>title1: content1</div><div>title2: content2</div></div>')
+      expect(rendered).to eq(
+        '<div class="notification-renderer notifications"><div>title1: '\
+        'content1</div><div>title2: content2</div></div>'
+      )
     end
   end
 end
 
 def create_title_and_content_notification(title, content)
-  Notification.create(type: 'title_and_content', metadata: {title: title, content: content})
+  Notification.create(type: 'title_and_content',
+                      metadata: { title: title, content: content })
 end
