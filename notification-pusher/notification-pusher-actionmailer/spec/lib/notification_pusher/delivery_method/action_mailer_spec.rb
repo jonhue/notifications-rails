7fd974301377c5d100f9cd1fba20bded717885d8
Jonas Hübotter <jonas.huebotter@gmail.com>
2020-08-18 14:44:41 +0200

    address rubocop offenses

--- notification-pusher/notification-pusher-actionmailer/spec/lib/notification_pusher/delivery_method/action_mailer_spec.rb
+++ notification-pusher/notification-pusher-actionmailer/spec/lib/notification_pusher/delivery_method/action_mailer_spec.rb
@@ -6,7 +6,9 @@ require_relative '../../../../lib/notification_pusher/delivery_method/' \
                  'action_mailer'
 
 RSpec.describe NotificationPusher::DeliveryMethod::ActionMailer do
+  # rubocop:disable RSpec/EmptyExampleGroup
   describe '#call' do
     pending
   end
+  # rubocop:enable RSpec/EmptyExampleGroup
 end
